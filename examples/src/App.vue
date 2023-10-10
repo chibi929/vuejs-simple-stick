8d4f25c01ff44174ee71b51179ef144c8f28993b
chibi929 <ibihc929@gmail.com>
2023-10-10 22:34:14 +0900

    [Update] Add examples

---
+++ examples/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SimpleStick />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SimpleStick from '../../src/components/SimpleStick.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    SimpleStick,
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
